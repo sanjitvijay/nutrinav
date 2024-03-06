b3a247757f8aa72042cd6807ce7ea01187e85199
Sanjit Vijay <sanjitv@umich.edu>
2024-03-06 16:52:48 -0500

    Set up form text collection

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,19 +1,59 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function SignIn() {
     const navigate = useNavigate();
 
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const{ email, password } = formData;
+
+    const onChange = e => {
+        setFormData((prevState) => ({
+            ...prevState, [e.target.id]: e.target.value
+        }));
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        console.log('Email:', email);
+        console.log('Password:', password)
+    }
+    
     
     return (
         <div className="flex justify-center items-center h-screen">
             <form className="flex flex-col">
-                <label htmlFor="username" className="mb-2">Email:</label>
-                <input type="text" id="username" name="username" className="border border-black mb-2" />
 
-                <label htmlFor="password" className="mb-2">Password:</label>
-                <input type="password" id="password" name="password" className="border border-black mb-2" />
+                <label className="mb-2">Email:</label>
+                <input 
+                    type="text" 
+                    id="email" 
+                    value={email}
+                    className="border border-black mb-2"
+                    onChange={onChange}
+                />
 
-                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign In</button>
+                <label className="mb-2">Password:</label>
+                <input 
+                    type="text" 
+                    id="password" 
+                    value={password}
+                    className="border border-black mb-2" 
+                    onChange={onChange}
+                />
+
+                <button 
+                    type="submit" 
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={onSubmit}
+                >
+                    Sign In
+                </button>
 
                 <div className="flex justify-between mt-4">
                     <button className="text-blue-500 hover:underline mr-5">Forgot Password</button>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,20 +1,70 @@
-import { useNavigate } from "react-router-dom"; 
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const SignUp = () => {
+function SignUp() {
     const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        email: '',
+        username: '',
+        password: ''
+    });
+
+    const{ email, username, password } = formData;
+
+    const onChange = e => {
+        setFormData((prevState) => ({
+            ...prevState, [e.target.id]: e.target.value
+        }));
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        console.log('Email:', email);
+        console.log('Username:', username)
+        console.log('Password:', password)
+    }
+    
+    
     return (
         <div className="flex justify-center items-center h-screen">
             <form className="flex flex-col">
+
                 <label className="mb-2">Email:</label>
-                <input type="text" id="email" name="email" className="border border-black mb-2" />
+                <input 
+                    type="text" 
+                    id="email" 
+                    value={email}
+                    className="border border-black mb-2"
+                    onChange={onChange}
+                />
 
                 <label className="mb-2">Username:</label>
-                <input type="text" id="username" name="username" className="border border-black mb-2" />
+                <input 
+                    type="text" 
+                    id="username" 
+                    value={username}
+                    className="border border-black mb-2"
+                    onChange={onChange}
+                />
 
                 <label className="mb-2">Password:</label>
-                <input type="password" id="password" name="password" className="border border-black mb-2" />
+                <input 
+                    type="text" 
+                    id="password" 
+                    value={password}
+                    className="border border-black mb-2" 
+                    onChange={onChange}
+                />
 
-                <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Sign Up</button>
+                <button 
+                    type="submit" 
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={onSubmit}
+                >
+                    Sign Up
+                </button>
 
                 <div className="flex justify-center mt-4">
                     <button 
@@ -28,6 +78,6 @@ const SignUp = () => {
             
         </div>
     );
-};
+}
 
 export default SignUp;
