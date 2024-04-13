af681e30a89b304236b4821d18a2275e8be90eb8
ntibebu <123273236+ntibebu@users.noreply.github.com>
2024-04-13 03:46:46 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -13,47 +13,48 @@ import ManualEntryForm from './pages/ManualEntryForm';
 import Profile from './pages/Profile';
 import AuthRoute from './components/AuthRoute';
 import ReportBug from './pages/ReportBug';
-import {Toaster} from 'react-hot-toast';
-
+import Nate from './pages/Nate'; // Import the new Nate page
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-  return (
-    <>
-      <TopNavbar />
-      <div className="p-5 pt-20 pb-20">
-        <Toaster />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path='/forgot-password' element={<ForgotPassword/>} />
-          <Route path="/add-food" element={<AuthRoute />}>
-            <Route path="/add-food" element={<AddFood />} />
-          </Route>
-          <Route path="/dashboard" element ={<AuthRoute/>}>
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route>
-          <Route path="/log" element={<AuthRoute />}>
-            <Route path="/log" element={<Log />} />
-          </Route>
-          <Route path="/user-info" element={<AuthRoute />}>
-            <Route path="/user-info" element={<UserInfo />} />
-          </Route>
-          <Route path="/manual-entry" element={<AuthRoute />}>
-            <Route path="/manual-entry" element={<ManualEntryForm />} />
-          </Route>
-          <Route path="/profile" element={<AuthRoute />}>
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-          <Route path="/report-bug" element={<AuthRoute />}>
-            <Route path="/report-bug" element={<ReportBug />} />
-          </Route>
-
-        </Routes>
-      </div>
-      <BottomNavbar/>
-    </>
-  )
+    return (
+        <>
+            <TopNavbar />
+            <div className="p-5 pt-20 pb-20">
+                <Toaster />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path='/forgot-password' element={<ForgotPassword/>} />
+                    <Route path="/add-food" element={<AuthRoute />}>
+                        <Route path="/add-food" element={<AddFood />} />
+                    </Route>
+                    <Route path="/dashboard" element={<AuthRoute />}>
+                        <Route path="/dashboard" element={<Dashboard />} />
+                    </Route>
+                    <Route path="/log" element={<AuthRoute />}>
+                        <Route path="/log" element={<Log />} />
+                    </Route>
+                    <Route path="/user-info" element={<AuthRoute />}>
+                        <Route path="/user-info" element={<UserInfo />} />
+                    </Route>
+                    <Route path="/manual-entry" element={<AuthRoute />}>
+                        <Route path="/manual-entry" element={<ManualEntryForm />} />
+                    </Route>
+                    <Route path="/profile" element={<AuthRoute />}>
+                        <Route path="/profile" element={<Profile />} />
+                    </Route>
+                    <Route path="/report-bug" element={<AuthRoute />}>
+                        <Route path="/report-bug" element={<ReportBug />} />
+                    </Route>
+                    <Route path="/nate" element={<Nate />} /> {/* New route for Nate page */}
+                </Routes>
+            </div>
+            <BottomNavbar/>
+        </>
+    )
 }
 
 export default App;
+
