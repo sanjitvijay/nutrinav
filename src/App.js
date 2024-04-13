649639c4d3c30125bea78c55158784bb52f20637
ntibebu <123273236+ntibebu@users.noreply.github.com>
2024-04-13 17:34:25 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,71 +1,11 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SignIn from './pages/SignIn';
-import Home from './pages/Home';
-import TopNavbar from './components/TopNavbar';
-import SignUp from './pages/SignUp';
-import AddFood from './pages/AddFood';
-import Dashboard from './pages/Dashboard';
-import BottomNavbar from './components/BottomNavbar';
-import Log from './pages/Log';
-import ForgotPassword from './pages/ForgotPassword';
-import UserInfo from './pages/UserInfo';
-import ManualEntryForm from './pages/ManualEntryForm';
-import Profile from './pages/Profile';
-import AuthRoute from './components/AuthRoute';
-import ReportBug from './pages/ReportBug';
-import CalorieCalc from './pages/CalorieCalc';
-import MoreFoodInfo from './pages/MoreFoodInfo';
-import ScannerPage from './pages/ScannerPage';
-import {Toaster} from 'react-hot-toast';
-
+import logo from './logo.svg';
+import './App.css';
+import Scanner from './components/Scanner';
 
 function App() {
   return (
-    <>
-      <TopNavbar />
-      <div className="p-5 pt-20 pb-20">
-        <Toaster />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path='/forgot-password' element={<ForgotPassword/>} />
-          <Route path="/add-food" element={<AuthRoute />}>
-            <Route path="/add-food" element={<AddFood />} />
-          </Route>
-          <Route path="/dashboard" element ={<AuthRoute/>}>
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route>
-          <Route path="/log" element={<AuthRoute />}>
-            <Route path="/log" element={<Log />} />
-          </Route>
-          <Route path="/user-info" element={<AuthRoute />}>
-            <Route path="/user-info" element={<UserInfo />} />
-          </Route>
-          <Route path="/manual-entry" element={<AuthRoute />}>
-            <Route path="/manual-entry" element={<ManualEntryForm />} />
-          </Route>
-          <Route path="/profile" element={<AuthRoute />}>
-            <Route path="/profile" element={<Profile />} />
-          </Route>
-          <Route path="/report-bug" element={<AuthRoute />}>
-            <Route path="/report-bug" element={<ReportBug />} />
-          </Route>
-          <Route path="/calorie-calc" element={<AuthRoute />}>
-            <Route path="/calorie-calc" element={<CalorieCalc />} />
-          </Route>
-          <Route path="/more-info" element={<AuthRoute />}>
-            <Route path="/more-info" element={<MoreFoodInfo />} />
-          </Route>
-          <Route path="/scanner" element={<AuthRoute />}>
-            <Route path="/scanner" element={<ScannerPage />} />
-          </Route>
-
-        </Routes>
-      </div>
-      <BottomNavbar/>
-    </>
-  )
+    <Scanner />
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,20 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AuthProvider from './context/AuthProvider';
-import UserInfoProvider from './context/UserInfoProvider';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <UserInfoProvider>
-          <App />
-        </UserInfoProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
