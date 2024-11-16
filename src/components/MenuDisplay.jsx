8d199552cad46e2ea3d9d56eebddfce455fde64b
ntibebu <ntibebu@umich.edu>
2024-11-16 17:37:27 -0500

    item format

--- src/components/MenuDisplay.jsx
+++ src/components/MenuDisplay.jsx
@@ -7,7 +7,7 @@ function MenuDisplay({ menu, time }) {
       {Object.keys(menu).map((subheader) => (
         <div key={subheader}>
           <h2 className="text-xl font-bold mt-4">{subheader}</h2>
-          <div className="grid grid-cols-1 gap-4">
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
             {menu[subheader].map((item) => (
               <ItemCard key={item.id} item={item} time={time} />
             ))}
